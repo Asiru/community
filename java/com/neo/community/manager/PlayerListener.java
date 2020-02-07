b12c8a20772088f7b077bbf05e128df6fabfabac
Asiru <12991517+Asiru@users.noreply.github.com>
2020-02-06 19:47:06 -0500

    Added display in chat for community scores

--- java/com/neo/community/manager/PlayerListener.java
+++ java/com/neo/community/manager/PlayerListener.java
@@ -1,9 +1,12 @@
 package com.neo.community.manager;
 
 import com.neo.community.Community;
+import com.neo.community.util.Utils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 
 public class PlayerListener implements Listener {
@@ -13,7 +16,7 @@ public class PlayerListener implements Listener {
 		this.plugin = plugin;
 	}
 	
-	@EventHandler
+	@EventHandler(ignoreCancelled = true)
 	public void onPlayerKick(PlayerKickEvent event) {
 		Player kicked = event.getPlayer();
 		String key = kicked.getUniqueId().toString();
@@ -22,5 +25,14 @@ public class PlayerListener implements Listener {
 		} else {
 			plugin.getPlayerDataStorage().addScore(key, plugin.getSettings().getPointsOnKick());
 		}
+	}
+	
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
+	public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
+		String format = event.getFormat();
+		String key = event.getPlayer().getUniqueId().toString();
+		double score = plugin.getPlayerDataStorage().getScore(key);
+		format = format.replace("{SCORE}", Utils.formatPoints(score));
+		event.setFormat(format);
 	}
 }
